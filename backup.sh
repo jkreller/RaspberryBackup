edc51510e06a5e78d7c070e5fc61b9804603e047
Julian Kreller <jkreller@eos-uptrade.de>
2020-02-16 01:43:15 +0100

    - Change language to English - Add variables - Add error message - Remove sending mail

--- backup.sh
+++ backup.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
-# Quelle des Skripts: https://raspberry.tips/raspberrypi-einsteiger/raspberry-pi-datensicherung-erstellen
-# Modifiziert von Lukas Knoeller (hobbyblogging.de)
-# Vor Nutzung bitte die Pfade entsprechend anpassen und die E-Mailadresse richtig setzen
-# Zum Senden von E-Mails auf dem Raspberry kann das Paket ssmtp genutzt werden
+# Source: https://raspberry.tips/raspberrypi-einsteiger/raspberry-pi-datensicherung-erstellen and https://hobbyblogging.de/raspberry-pi-vollautomatisch-sichern
+# Modified by Lukas Knoeller (hobbyblogging.de) and Julian Kreller
+# Forked from https://github.com/lkn94/RaspberryBackup
 
-#Festplatte einbinden
-mount -t cifs -o user=USERNAME,password=PASSWORD,rw,file_mode=0777,dir_mode=0777 //IP/FREIGABE /mnt/nas
+#Variables
+NAS_USER="user" #Change this to your NAS username
+NAS_PW="password" #Change this to your NAS password
+NAS_ADDRESS="192.168.0.123" #Change this to your NAS address
+NAS_BACKUP_PATH="/directory/to/backups" #Change this to the path of the backups directory on your NAS
 
-#Variablen
-BACKUP_PFAD=”/mnt/nas/Backup”
-BACKUP_ANZAHL=”5”
-BACKUP_NAME=”Sicherung”
+BACKUP_PATH="/mnt/nas"
+BACKUP_NAME="backup"
 
-#Backup erstellen
-dd if=/dev/mmcblk0 of=${BACKUP_PFAD}/${BACKUP_NAME}-$(date +%Y%m%d).img bs=1MB
+BACKUP_FILE="${BACKUP_PATH}/${BACKUP_NAME}_$(date +%Y-%m-%d).img"
 
-#Alte Sicherung löschen
-pushd ${BACKUP_PFAD}; ls -tr ${BACKUP_PFAD}/${BACKUP_NAME}* | head -n -${BACKUP_ANZAHL} | xargs rm; popd
+#Mount NAS
+mount -t "cifs" -o "user=${NAS_USER},password=${NAS_PW},rw,file_mode=0777,dir_mode=0777" "//${NAS_ADDRESS}${NAS_BACKUP_PATH}" "${BACKUP_PATH}"
 
-#Festplatte auswerfen
-umount /mnt/nas
+#Check if backup file exists already
+if [ -f "${BACKUP_FILE}" ]
+then
+    echo -e "\e[91m\xe2\x9c\x98 Backup file ${BACKUP_FILE} exists already"
+    exit 1
+fi
 
-#Info versenden
-to="mail@example.com"
-servername="NAME or IP"
-mail -s "Backup Skript" -t $to <<< "Der Server ($servername) hat eben das Backup-Skript ausgeführt."
+#Create backup
+dd if="/dev/mmcblk0" of="${BACKUP_FILE}" bs="1MB"
+
+#Remove backups older than 90 days
+find "${BACKUP_PATH}/${BACKUP_NAME}"* -type f -mtime +90 -exec rm {} \;
+
+#Unmount NAS
+umount "${BACKUP_PATH}"
+
+if [ ${?} == 0 ]
+then
+    echo -e "\e[92m\xE2\x9C\x94 Backup file created successfully: ${BACKUP_FILE}"
+fi
